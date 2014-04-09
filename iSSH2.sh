aa5235c4c4fb7900d9a70c6a7851e65fab54e14a
Tommaso Madonia <frugghi@gmail.com>
2014-04-09 18:59:10 +0200

    Automatically detect SDK version

--- iSSH2.sh
+++ iSSH2.sh
@@ -26,31 +26,27 @@
                                #################
 ################################ Configuration #################################
 #                              #################                               #
- LIBSSL_VERSION="1.0.1e"
+ LIBSSL_VERSION="1.0.1g"
  LIBSSH_VERSION="1.4.3"
- SDK_VERSION="7.0"
 ################################################################################
 
 echo "Initializing..."
 
 export LIBSSL_VERSION
 export LIBSSH_VERSION
-export SDK_VERSION
 
 export IPHONEOS_MINVERSION="6.0"
 export ARCHS="i386 x86_64 armv7 armv7s arm64"
 
-export BASEPATH=`pwd`
+export BASEPATH="${PWD}"
 export LIBSSLDIR="${BASEPATH}/tmp/openssl-${LIBSSL_VERSION}"
 export LIBSSHDIR="${BASEPATH}/tmp/libssh2-${LIBSSH_VERSION}"
 
-export CLANG=`xcrun -find clang`
-export DEVELOPER=`xcode-select -print-path`
+export SDK_VERSION=`xcrun --sdk iphoneos --show-sdk-version`
+export CLANG=`xcrun --find clang`
+export DEVELOPER=`xcode-select --print-path`
 
 set -e
 
-echo "Building openssl ${LIBSSL_VERSION}:"
 ./iSSH2-openssl.sh
-
-echo "Building libssh2 ${LIBSSH_VERSION}:"
 ./iSSH2-libssh2.sh
